d40011662aa8e76d4025878ed8f920c41b826223
Cache Angus <15sa54@caslab.queensu.ca>
2018-10-29 20:54:05 -0400

    Gui Stuff

---
+++ initialGui/MyApplication/app/src/main/java/com/example/cacheangus/myapplication/CameraActivity.java
@@ -0,0 +1,14 @@
+package com.example.cacheangus.myapplication;
+
+
+import android.os.Bundle;
+
+
+
+public class CameraActivity extends MainActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_camera);
+    }
+}
