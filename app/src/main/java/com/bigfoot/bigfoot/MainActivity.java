8e4ed39403d5cae9873f4480930548c5b65fe348
parker <12tpjp@caslab.queensu.ca>
2018-11-11 17:52:58 -0500

    Put functionality for the scan page into the bitbucket. Others should be able to use this is as a starting point

--- app/src/main/java/com/bigfoot/bigfoot/MainActivity.java
+++ app/src/main/java/com/bigfoot/bigfoot/MainActivity.java
@@ -3,27 +3,30 @@ package com.bigfoot.bigfoot;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    // Used to load the 'native-lib' library on application startup.
-    static {
-        System.loadLibrary("native-lib");
-    }
+    public static TextView tvresult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Example of a call to a native method
-        TextView tv = (TextView) findViewById(R.id.sample_text);
-        tv.setText(stringFromJNI());
-    }
+        tvresult = (TextView) findViewById(R.id.tvresult);
 
-    /**
-     * A native method that is implemented by the 'native-lib' native library,
-     * which is packaged with this application.
-     */
-    public native String stringFromJNI();
+        Button btn = (Button) findViewById(R.id.btn);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ScanActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
 }
