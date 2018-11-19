02716ee22ebe5478814cc53984f41bd14d6151e4
oppenheimer <15kao2@caslab.queensu.ca>
2018-11-18 22:41:15 -0500

    Fixing the issue in the native-lib.

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,16 +1,10 @@
 #include <jni.h>
 #include <string>
-using namespace std;
+
 extern "C" JNIEXPORT jstring JNICALL
 Java_com_bigfoot_bigfoot_MainActivity_stringFromJNI(
         JNIEnv *env,
         jobject /* this */) {
-    string hello = "Hello from C++";
-    return env->NewStringUTF(hello.c_str());
-}
-
-extern "C" JNIEXPORT jstring JNICALL
-Java_com_bigfoot_bigfoot_MainActivity_instructions() {
-    string hello = "Hello from C++";
+    std::string hello = "Hello from C++";
     return env->NewStringUTF(hello.c_str());
 }
