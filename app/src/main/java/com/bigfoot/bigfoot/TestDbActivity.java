d328a622f92af91ba3dde4a46607d1463accd6e2
chrisc <18cwc2@queensu.ca>
2018-11-20 09:31:28 -0500

    tested database connection WE CAN SSH

---
+++ app/src/main/java/com/bigfoot/bigfoot/TestDbActivity.java
@@ -0,0 +1,13 @@
+package com.bigfoot.bigfoot;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TestDbActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test_db);
+    }
+}
