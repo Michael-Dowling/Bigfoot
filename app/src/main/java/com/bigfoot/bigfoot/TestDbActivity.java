09de0c23a0a7d31cdae713389ad22d4d8378a585
parker <12tpjp@caslab.queensu.ca>
2018-11-20 10:34:59 -0500

    CHanged SSH to SQL in TestDBACtivity, should select * from BFItem

--- app/src/main/java/com/bigfoot/bigfoot/TestDbActivity.java
+++ app/src/main/java/com/bigfoot/bigfoot/TestDbActivity.java
@@ -9,6 +9,8 @@ import com.jcraft.jsch.JSchException;
 import com.jcraft.jsch.Session;
 import android.widget.Button;
 import android.widget.TextView;
+import java.sql.*;
+
 
 public class TestDbActivity extends AppCompatActivity {
     @Override
@@ -16,18 +18,18 @@ public class TestDbActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test_db);
 
-        //TextView textView = findViewById(R.id.displayText);
+        TextView textView = findViewById(R.id.displayText);
 
         Button connectBtn = findViewById(R.id.connectButton);
         connectBtn.setOnClickListener(new View.OnClickListener() {
-           // @SuppressLint("StaticFieldLeak")
+            // @SuppressLint("StaticFieldLeak")
             @Override
-            public void onClick(View v){
-                new AsyncTask<Integer, Void, Void>(){
+            public void onClick(View v) {
+                new AsyncTask<Integer, Void, Void>() {
                     @Override
                     protected Void doInBackground(Integer... params) {
                         try {
-                            executeSSHcommand();
+                            executeSQLcmds();
                         } catch (Exception e) {
                             e.printStackTrace();
                         }
@@ -38,79 +40,73 @@ public class TestDbActivity extends AppCompatActivity {
         });
     }
 
-    public void executeSSHcommand(){
+    public void executeSSHcommand() {
         String user = "albert";
         String password = "Fugei0cahcah";
         String host = "130.15.15.4";
-        int port=22;
-        try{
+        int port = 22;
+        TextView textView = findViewById(R.id.displayText);
+        try {
 
             JSch jsch = new JSch();
             Session session = jsch.getSession(user, host, port);
             session.setPassword(password);
             session.setConfig("StrictHostKeyChecking", "no");
-            session.setTimeout(10000);
+            session.setTimeout(1000);
             session.connect();
-            ChannelExec channel = (ChannelExec)session.openChannel("exec");
-            channel.setCommand("echo 'hello' > YASSS.txt");
+            ChannelExec channel = (ChannelExec) session.openChannel("exec");
+            channel.setCommand("echo 'hello' > Yesssssssss.txt");
             channel.connect();
             channel.disconnect();
             // show success in UI with a snackbar alternatively use a toast
 //            Snackbar.make(getActivity().findViewById(android.R.id.content),
 //                    "Success!", Snackbar.LENGTH_LONG)
-//                    .setAction("Action", null).show();
+//                    .setAction("Act.ion", null).show();
 
-           // text.setText("success!");
+           // textView.setText("success!");
 
-        }
-        catch(JSchException e){
-            // show the error in the UI
-//            Snackbar.make(findViewById(android.R.id.content),
-//                    "Check WIFI or Server! Error : "+e.getMessage(),
-//                    Snackbar.LENGTH_LONG)
-//                    .setDuration(20000).setAction("Action", null).show();
+        } catch (JSchException e) {
+            textView.setText(e.toString());
+            // textView.setText("Failure");
 
         }
     }
-}
 
-//    public void executeSSLcmds() {
-//
-//
-//
-// try {
-//            go();
-//        } catch (Exception ex) {
-//            ex.printStackTrace();
-//        }
-//        System.out.println("An example for updating a Row from Mysql Database!");
-//        Connection con = null;
-//        String driver = "com.mysql.jdbc.Driver";
-//        String url = "jdbc:mysql://" + rhost + ":" + lport + "/";
-//        String db = "Bigfootdb";
-//        String dbUser = "albert";
-//        String dbPasswd = "Fugei0cahcah";
-//        try {
-//            Class.forName(driver);
-//            con = DriverManager.getConnection(url + db, dbUser, dbPasswd);
-//            try {
-//                Statement st = con.createStatement();
-//                String sql = "UPDATE MyTableName " +
-//                        "SET email = 'ripon.wasim@smile.com' WHERE email='peace@happy.com'";
-//
-//                int update = st.executeUpdate(sql);
-//                if (update >= 1) {
-//                    System.out.println("Row is updated.");
-//                } else {
-//                    System.out.println("Row is not updated.");
-//                }
-//            } catch (SQLException s) {
-//                System.out.println("SQL statement is not executed!");
-//            }
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//    }
+
+    public void executeSQLcmds() {
+        System.out.println("An example for updating a Row from Mysql Database!");
+        Connection con = null;
+        String driver = "com.mysql.jdbc.Driver";
+        String rhost = "albert.caslab.queensu.ca";
+        int lport = 3306;
+        String url = "jdbc:mysql://" + rhost + ":" + lport + "/";
+        String db = "bigfoot";
+        String dbUser = "biguser";
+        String dbPasswd = "bigfoot";
+        TextView textView = findViewById(R.id.displayText);
+        try {
+            Class.forName(driver);
+            con = DriverManager.getConnection(url + db, dbUser, dbPasswd);
+            try {
+                Statement stmt = con.createStatement();
+                String sql = "Select * From BFItem";
+
+                ResultSet rs = stmt.executeQuery(sql);
+                String barcodes = "";
+                while (rs.next()) {
+                    String barcodeNumber = rs.getString("barcodeNumber");
+                    barcodes += barcodeNumber;
+                    barcodes += " ";
+                }
+                textView.setText(barcodes);
+            } catch (SQLException s) {
+                System.out.println("SQL statement is not executed!");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
 
 
 
