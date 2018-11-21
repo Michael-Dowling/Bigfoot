f9a5a1fc71b299eaae1a171babee9d5d58f1e447
chrisc <18cwc2@queensu.ca>
2018-11-20 23:45:53 -0500

    the database LIVES

--- app/src/main/java/com/bigfoot/bigfoot/TestDbActivity.java
+++ app/src/main/java/com/bigfoot/bigfoot/TestDbActivity.java
@@ -1,123 +1,156 @@
 package com.bigfoot.bigfoot;
 import android.os.AsyncTask;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import com.jcraft.jsch.ChannelExec;
-import com.jcraft.jsch.JSch;
-import com.jcraft.jsch.JSchException;
-import com.jcraft.jsch.Session;
-import android.widget.Button;
-import android.widget.TextView;
-import java.sql.*;
-import java.sql.DriverManager;
-import com.mysql.jdbc.Driver;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class TestDbActivity extends AppCompatActivity {
 
-    public static TextView textView;
+    ListView listView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test_db);
-        textView = findViewById(R.id.displayText);
-        textView.setText("CHANGE");
 
-        Button connectBtn = findViewById(R.id.connectButton);
-        connectBtn.setOnClickListener(new View.OnClickListener() {
-            // @SuppressLint("StaticFieldLeak")
+                listView = (ListView) findViewById(R.id.listView);
+                downloadJSON("http://albert.caslab.queensu.ca/bigfootService.php");
+            }
+
+    private void downloadJSON(final String urlWebService) {
+
+        class DownloadJSON extends AsyncTask<Void, Void, String> {
+
             @Override
-            public void onClick(View v) {
-                textView.setText("push the BUTTON");
-                new AsyncTask<Integer, Void, Void>() {
-                    @Override
-                    protected Void doInBackground(Integer... params) {
-                        try {
-                            executeSQLcmds();
-                           // textView.setText("we did it");
-                        } catch (Exception e) {
-                            e.printStackTrace();
-                        }
-                        return null;
-                    }
-                }.execute(1);
+            protected void onPreExecute() {
+                super.onPreExecute();
             }
-        });
-    }
-
-    public void executeSSHcommand() {
-        String user = "albert";
-        String password = "Fugei0cahcah";
-        String host = "130.15.15.4";
-        int port = 22;
-        TextView textView = findViewById(R.id.displayText);
-        try {
-
-            JSch jsch = new JSch();
-            Session session = jsch.getSession(user, host, port);
-            session.setPassword(password);
-            session.setConfig("StrictHostKeyChecking", "no");
-            session.setTimeout(1000);
-            session.connect();
-            ChannelExec channel = (ChannelExec) session.openChannel("exec");
-            channel.setCommand("echo 'hello' > Yesssssssss.txt");
-            channel.connect();
-            channel.disconnect();
-            // show success in UI with a snackbar alternatively use a toast
-//            Snackbar.make(getActivity().findViewById(android.R.id.content),
-//                    "Success!", Snackbar.LENGTH_LONG)
-//                    .setAction("Act.ion", null).show();
-
-           // textView.setText("success!");
-
-        } catch (JSchException e) {
-            textView.setText(e.toString());
-            // textView.setText("Failure");
-
-        }
-    }
 
 
-    public void executeSQLcmds() {
-        System.out.println("An example for updating a Row from Mysql Database!");
-        Connection con = null;
-        String driver = "com.mysql.jdbc.Driver";
-        String rhost = "130.15.15.4";
-
-        int lport = 22;
-        String url = "jdbc:mysql://" + rhost + ":" + lport + "/";
-        String db = "bigfoot";
-        String dbUser = "biguser";
-        String dbPasswd = "bigfoot";
-        textView.setText("set variables");
-
-        try {
-            Class.forName(driver);
-            con = DriverManager.getConnection(url + db, dbUser, dbPasswd);
-            textView.setText("connected");
-            try {
-                Statement stmt = con.createStatement();
-                String sql = "Select * From BFItem";
-
-                ResultSet rs = stmt.executeQuery(sql);
-                String barcodes = "";
-                while (rs.next()) {
-                    String barcodeNumber = rs.getString("barcodeNumber");
-                    barcodes += barcodeNumber;
-                    barcodes += " ";
+            @Override
+            protected void onPostExecute(String s) {
+                super.onPostExecute(s);
+                Toast.makeText(getApplicationContext(), s, Toast.LENGTH_SHORT).show();
+                try {
+                    loadIntoListView(s);
+                } catch (JSONException e) {
+                    e.printStackTrace();
                 }
-                //textView.setText(barcodes);
-            } catch (SQLException s) {
-                System.out.println("SQL statement is not executed!");
             }
-        } catch (Exception e) {
-            textView.setText(e.toString());
-            Log.e("woo", e.toString());
+
+            @Override
+            protected String doInBackground(Void... voids) {
+                try {
+                    URL url = new URL(urlWebService);
+                    HttpURLConnection con = (HttpURLConnection) url.openConnection();
+                    StringBuilder sb = new StringBuilder();
+                    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(con.getInputStream()));
+                    String json;
+                    while ((json = bufferedReader.readLine()) != null) {
+                        sb.append(json + "\n");
+                    }
+                    return sb.toString().trim();
+                } catch (Exception e) {
+                    return null;
+                }
+            }
         }
+        DownloadJSON getJSON = new DownloadJSON();
+        getJSON.execute();
     }
+
+    private void loadIntoListView(String json) throws JSONException {
+        JSONArray jsonArray = new JSONArray(json);
+        String[] result = new String[jsonArray.length()];
+        for (int i = 0; i < jsonArray.length(); i++) {
+            JSONObject obj = jsonArray.getJSONObject(i);
+            result[i] = obj.getString("itemName") + " " + obj.getString("barcodeNumber");
+        }
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, result);
+        listView.setAdapter(arrayAdapter);
+    }
+//    public void executeSSHcommand() {
+//        String user = "albert";
+//        String password = "Fugei0cahcah";
+//        String host = "130.15.15.4";
+//        int port = 22;
+//        TextView textView = findViewById(R.id.displayText);
+//        try {
+//
+//            JSch jsch = new JSch();
+//            Session session = jsch.getSession(user, host, port);
+//            session.setPassword(password);
+//            session.setConfig("StrictHostKeyChecking", "no");
+//            session.setTimeout(1000);
+//            session.connect();
+//            ChannelExec channel = (ChannelExec) session.openChannel("exec");
+//            channel.setCommand("echo 'hello' > Yesssssssss.txt");
+//            channel.connect();
+//            channel.disconnect();
+//            // show success in UI with a snackbar alternatively use a toast
+////            Snackbar.make(getActivity().findViewById(android.R.id.content),
+////                    "Success!", Snackbar.LENGTH_LONG)
+////                    .setAction("Act.ion", null).show();
+//
+//           // textView.setText("success!");
+//
+//        } catch (JSchException e) {
+//            textView.setText(e.toString());
+//            // textView.setText("Failure");
+//
+//        }
+//    }
+
+
+//    public void executeSQLcmds() {
+//        System.out.println("An example for updating a Row from Mysql Database!");
+//        Connection con = null;
+//        String driver = "com.mysql.jdbc.Driver";
+//        String rhost = "130.15.15.4";
+//
+//        int lport = 1433;
+//        String url = "jdbc:mysql://" + rhost + ":" + lport + "/";
+//        String db = "bigfoot";
+//        String dbUser = "biguser";
+//        String dbPasswd = "bigfoot";
+//        textView.setText("set variables");
+//
+//        try {
+//            Class.forName(driver);
+//            con = DriverManager.getConnection(url + db, dbUser, dbPasswd);
+//            textView.setText("connected");
+//            try {
+//                Statement stmt = con.createStatement();
+//                String sql = "Select * From BFItem";
+//
+//                ResultSet rs = stmt.executeQuery(sql);
+//                String barcodes = "";
+//                while (rs.next()) {
+//                    String barcodeNumber = rs.getString("barcodeNumber");
+//                    barcodes += barcodeNumber;
+//                    barcodes += " ";
+//                }
+//                textView.setText(barcodes);
+//            } catch (SQLException s) {
+//                System.out.println("SQL statement is not executed!");
+//            }
+//        } catch (Exception e) {
+//           textView.setText(e.toString());
+//            Log.e("woo", e.toString());
+//        }
+//    }
+
+
 }
-
-
 
