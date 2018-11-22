07779f5727f4e98cf9a4b1548c8c151a6731e6a0
chrisc <18cwc2@queensu.ca>
2018-11-22 17:58:33 -0500

    Chris and Mike fixed the search bar and other small bugs

---
+++ app/src/main/java/com/bigfoot/bigfoot/SearchResultsActivity.java
@@ -0,0 +1,29 @@
+package com.bigfoot.bigfoot;
+
+import android.app.SearchManager;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class SearchResultsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_results);
+        handleIntent(getIntent());
+    }
+    @Override
+    protected void onNewIntent(Intent intent) {
+        handleIntent(intent);
+    }
+
+    private void handleIntent(Intent intent) {
+
+        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+            String query = intent.getStringExtra(SearchManager.QUERY);
+            Toast.makeText(getApplicationContext(),query,Toast.LENGTH_SHORT).show();
+        }
+    }
+}
